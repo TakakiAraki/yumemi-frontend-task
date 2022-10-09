f6309e61afc1cfe47847d6b3b11d102e629d8784
TakakiAraki <sjyyj008@gmail.com>
2022-10-09 14:59:11 +0900

    fix: 前置詞にisがあるため、オブジェクトパラメータのisを除去

--- src/utils/Is.ts
+++ src/utils/Is.ts
@@ -1,7 +1,7 @@
 export const is = {
-  isProduction: process.env.NODE_ENV === "production",
-  isDevelopment: process.env.NODE_ENV === "development",
-  isTest: process.env.NODE_ENV === "test",
+  production: process.env.NODE_ENV === "production",
+  development: process.env.NODE_ENV === "development",
+  test: process.env.NODE_ENV === "test",
   null: <T extends unknown>(value: T | null | undefined): value is T => value == null,
   notNull: <T extends unknown>(value: T | null | undefined): value is T => value != null,
 };
