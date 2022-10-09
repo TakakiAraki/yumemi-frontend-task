2f64cd66fcf905e09199b9c8b1c83147ee7ebe21
TakakiAraki <sjyyj008@gmail.com>
2022-10-09 18:13:36 +0900

    feat: サンプルのテキストコンポーネントを定義

---
+++ src/stories/components/common/Text.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta } from "@storybook/react";
+import { Text } from "components/common/text/Text";
+
+export const Primary = {};
+export default {
+  component: Text,
+  argTypes: {
+    children: {
+      name: "children",
+      type: "string",
+      defaultValue: "hoge",
+    },
+  },
+} as ComponentMeta<typeof Text>;
