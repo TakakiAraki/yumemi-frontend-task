2e0bbb5d54743326e2fe5a074e8364ea6265daa5
TakakiAraki <sjyyj008@gmail.com>
2022-10-09 21:20:19 +0900

    feat: グラフライブラリを用いて一旦描画

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { useMachine } from "@xstate/react";
 import { ExampleMachine } from "~/stores/example/Example";
+import dynamic from "next/dynamic";
 
 // SSR Fetch
 // import fetch from 'isomorphic-unfetch';
@@ -9,6 +10,9 @@ type IIndexProps = {
   name: string;
 };
 
+const ExampleChart = dynamic(() => import("~/components/common/graph/ExampleChart"), {
+  ssr: false,
+});
 const index = (props: IIndexProps) => {
   /* props */
   const { name } = props;
@@ -19,6 +23,48 @@ const index = (props: IIndexProps) => {
       <div>
         <h1>{name}</h1>
       </div>
+      {/* 
+        横軸に関しては
+      */}
+      <ExampleChart
+        data={[
+          {
+            date: "2019",
+            values: {
+              静岡県: 1000,
+              神奈川県: 1101,
+            },
+          },
+          {
+            date: "2020",
+            values: {
+              静岡県: 1000,
+              神奈川県: 1101,
+            },
+          },
+          {
+            date: "2021",
+            values: {
+              静岡県: null,
+              神奈川県: 1201,
+            },
+          },
+          {
+            date: "2022",
+            values: {
+              静岡県: 1200,
+              神奈川県: 1501,
+            },
+          },
+          {
+            date: "2023",
+            values: {
+              静岡県: 1200,
+              神奈川県: 1501,
+            },
+          },
+        ]}
+      />
       <button onClick={() => send("fetch")} disabled={!current.matches("initial")}>
         send fetch
       </button>
