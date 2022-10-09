6f7a89cc2a1257e17de1b3ecd1f5c8b7f3c38bed
TakakiAraki <sjyyj008@gmail.com>
2022-10-10 01:54:15 +0900

    feat: filterMapを実装

---
+++ src/utils/array.ts
@@ -0,0 +1,22 @@
+import { is } from "./Is";
+
+type FilterCb<Value extends unknown, Result extends unknown> = (
+  value: Value,
+  index: number,
+  array: Value[],
+) => false | null | undefined | Result;
+
+export const reduce = {
+  filterMap:
+    <Value extends unknown, Result extends unknown>(cb: FilterCb<Value, Result>) =>
+    (
+      previousValue: Result[],
+      currentValue: Value,
+      currentIndex: number,
+      array: Value[],
+    ): Result[] => {
+      const value = cb(currentValue, currentIndex, array);
+      if ((typeof value === "boolean" && !value) || is.null(value)) return previousValue;
+      return [...previousValue, value];
+    },
+};
