7dbf44317b2c7c811744555e3b5f87a4a80c0e8f
TakakiAraki <sjyyj008@gmail.com>
2022-10-10 19:11:15 +0900

    fix: @typesはlint対象外

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,7 @@ module.exports = {
     },
   },
   parser: "@typescript-eslint/parser",
-  ignorePatterns: ["**/*.md", "**/*.scss", "**/*.mdx", "**/*.svg"],
+  ignorePatterns: ["**/*.md", "**/*.scss", "**/*.mdx", "**/*.svg", "**.d.ts"],
   rules: {
     "no-console": "warn",
     "no-undef": "off",
