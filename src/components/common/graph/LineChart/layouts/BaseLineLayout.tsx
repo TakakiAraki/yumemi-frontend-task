bd0b8385a077c8a5c5b157a4060733acf2330eab
TakakiAraki <sjyyj008@gmail.com>
2022-10-14 16:13:00 +0900

    fix: 命名を一定させた

--- src/components/common/graph/LineChart/layouts/BaseLineLayout.tsx
+++ src/components/common/graph/LineChart/layouts/BaseLineLayout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Accordion } from "~/components/common/accordion/Accordion";
 import { LineDescription } from "../components/LineDescription";
 import { LineGraphCheckList } from "../components/LineGraphCheckList";
-import { LineTitle } from "../components/LineTitle";
+import { LineTitle } from "../components/LinegraphTitle";
 import style from "./BaseLineLayout.module.scss";
 
 const LineGraph = dynamic(
