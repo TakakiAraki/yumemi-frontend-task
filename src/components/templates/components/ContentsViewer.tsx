aed21b33ed1beb039062988c55f6079ab3cf757f
TakakiAraki <sjyyj008@gmail.com>
2022-10-16 17:55:19 +0900

    fix: 不要な処理を除去

--- src/components/templates/components/ContentsViewer.tsx
+++ src/components/templates/components/ContentsViewer.tsx
@@ -6,17 +6,7 @@ import { useApplicationSelector } from "~/stores/applicatrion/store";
 
 const ContentsViewer: FC = () => {
   const dataGroup = useApplicationSelector((state) => state.dataGroup.resource);
-  const isCompleted = useApplicationSelector((state) => {
-    return state.graphMeta.state === "completed";
-  });
 
-  if (!isCompleted)
-    return (
-      <div>
-        hoge
-        <p>{isCompleted ? "true" : "false"}</p>
-      </div>
-    );
   return (
     <>
       <div className={styles["content-template"]}>
