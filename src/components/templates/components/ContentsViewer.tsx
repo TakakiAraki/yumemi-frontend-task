efa2b10badd1f074413033c69d689cd3a1c55dbf
TakakiAraki <sjyyj008@gmail.com>
2022-10-16 17:54:09 +0900

    fix: 不要なファイルを除去

--- src/components/templates/components/ContentsViewer.tsx
+++ src/components/templates/components/ContentsViewer.tsx
@@ -1,20 +1,8 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { BaseLineList } from "~/components/graphRecord/layout/BaseChartList";
 import styles from "./ContentsViewer.module.scss";
 import { FC } from "react";
-import { Chart2DMetaData } from "~/components/common/graph/LineChart/intarface";
-import { is } from "~/utils/Is";
-import { IndexProps } from "..";
 import { useApplicationSelector } from "~/stores/applicatrion/store";
-
-const validatinGraphTypes = (key: string, graphType: IndexProps): Chart2DMetaData => {
-  if (key === "demographics") {
-    const target = graphType[key];
-    if (is.null(target)) throw new Error(`グラフのメタデータが設定されていません ${target}`);
-    return target;
-  }
-  throw new Error(`グラフタイプが存在しません ${key}`);
-};
 
 const ContentsViewer: FC = () => {
   const dataGroup = useApplicationSelector((state) => state.dataGroup.resource);
